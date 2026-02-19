6fc949938b99b705d4f05afa807a0639aab014a2
divyansh-tripathi <divyanshtripathi201@gmail.com>
2026-02-19 22:54:02 +0530

    update navbar

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,3 @@
 // API Configuration
-export const API_BASE_URL = "http://localhost:4000";
+export const API_BASE_URL = "https://e-commerce-vfne.onrender.com" || "http://localhost:4000";
 // export const API_BASE_URL = "https://e-commerce-vfne.onrender.com";
